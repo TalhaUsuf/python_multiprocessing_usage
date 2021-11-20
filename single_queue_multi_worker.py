ab6dfa070f44ddcf0db0c44d8ff1cb6dea5c078d
TalhaUsuf <47290815+TalhaUsuf@users.noreply.github.com>
2021-11-20 17:10:16 +0500

    argparser added

---
+++ single_queue_multi_worker.py
@@ -0,0 +1,14 @@
+from multiprocessing import Process, Manager
+import argparse
+
+arg = argparse.ArgumentParser(description="single queueu multi worker")
+arg.add_argument('-d', '--dir', default='./', help='path to the dir having images')
+parser = arg.parse_args()
+
+
+
+
+
+if __name__=='__main__':
+    NotImplemented
+    
